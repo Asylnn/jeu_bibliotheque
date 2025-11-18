231c2df4103299826241b25afa1076a27a1e2276
Leticia <leticianapolitano34@gmail.com>
2025-11-18 17:08:50 +0100

    gestion entrees joueurs sorties

--- public/client.js
+++ public/client.js
@@ -3,7 +3,35 @@ console.log("hello world!")
 let socket = io()
 socket.emit("ping")
 socket.on("pong", data => {
-    console.log("pong")
+    console.log("recu pong")
+})
+
+
+function entrerDansLaPartie(){
+    let input=document.getElementById("nom");
+    socket.emit('entree', input.value);
+    console.log("entrer dans la partie")
+    
+    
+}
+
+function seDeconnecterDeLaPartie(){
+    let input=document.getElementById("nom");
+    socket.emit("sortie", input.value);
+    console.log("sortie de la partie");
+}
+
+
+socket.on("erreur", 
+    messageErreur => {
+        console.log(messageErreur)
+    }
+)
+
+socket.on("liste joueurs", noms => {
+    let elem=document.getElementById("joueurs")
+    elem.textContent=noms.toString()
+    console.log(noms)
 })
 
 socket.emit("envoie message chat", {message:"Salut ca va"})
--- server.js
+++ server.js
@@ -13,21 +13,108 @@ app.get('/', (req, res) => {
     res.sendFile("index.html", {root: __dirname})
 })
 
+class Joueur{
+    constructor(socketID, nom){
+        this.socketID=socketID;
+        this.nom=nom;
+    }
+}
+
+let dict_joueurs={}
+
+
+/*
 function message_chat(data)
 {
     console.log(data.message)
 }
 
+let objjavascript = {
+    abc: "string",
+    def: "string2"
+}
+
+d = "abc"
+objjavascript.abc == objjavascript[d]
+
+*/
+function sortiePartie(){
+    socket.on("sortie", (nom) => {
+        console.log("message sortie reçu")
+        delete dict_joueurs[socket.id]
+    })
+
+}
+
+function getNoms(){
+    jrs=Object.values(dict_joueurs)
+    noms=[]
+     for(let joueur of jrs){
+            noms.push(joueur.nom)
+        }
+   
+    return noms;
+}
 io.on("connect", (socket) => {
     console.log("nouvelle connection")
     socket.on("ping", () => {
         console.log("pong")
         socket.emit("pong")
+        
     })
+    jrs=Object.values(dict_joueurs)
+    noms=[]
+     for(let joueur of jrs){
+            noms.push(joueur.nom)
+        }
+    socket.emit("liste joueurs", getNoms())
+    
+
+    socket.on("sortie", sortiePartie)
+
+    
+
+    socket.on("entree", (nom) =>
+    {
+
+        console.log("message reçu")
+        jrs=Object.values(dict_joueurs)
+        for(let joueur of jrs){
+            if(joueur.nom == nom){
+                socket.emit("erreur", "Deux noms ne peuvent pas être identiques")
+                return;
+            } 
+        }
+
+        dict_joueurs[socket.id]= new Joueur(socket.id, nom)
+        socket.broadcast.emit("liste joueurs", getNoms())
+        
+        
+    })
+
     socket.on("envoie message chat", function (data) {
-        console.log(data.message)
-        io.broadcast("donner message chat", data.message)
+        console.log(data.message)//DZYDZIEYD
+  //      io.broadcast("donner message chat", data.message)
     })
     
-})
+}
+
+    
+
+)
+
+io.on("deconnecter", (socket) => {
+    console.log("deconnecter")
+    sortiePartie()
+
+  
+
+    
+}
+
+
+
+)
+
+
 
